d441aa4dd1f78f31aae3ced84e994d1e9be19825
Kieran Dunch <dunch.kieran@gmail.com>
2023-11-02 08:16:08 -0600

    navbar content

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,19 @@
 import { Navbar } from "react-bootstrap"
+import Nav from "react-bootstrap/Nav"
 
 export default function NavBar() {
   return (
     <Navbar bg="light" expand="lg">
       <Navbar.Brand href="#">SEUOL BEAUTY</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav>
+          <Nav.Link href="#">Home</Nav.Link>
+          <Nav.Link href="#">About</Nav.Link>
+          <Nav.Link href="#">Products</Nav.Link>
+          <Nav.Link href="#">Sign Up</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   )
 }
