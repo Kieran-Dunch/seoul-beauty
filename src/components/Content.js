bc21fb695a8a5f7404324447df808d3546a96d46
Kieran Dunch <dunch.kieran@gmail.com>
2023-11-08 08:07:24 -0700

    image carousel

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,42 @@
+import { Carousel, Col, Container, Image, Row } from "react-bootstrap";
+
 export default function Content() {
   return (
-    <div>Content</div>
+    <>
+      <Container>
+        <Carousel>
+          <Carousel.Item>
+            <Container>
+              <Row>
+                <Col>
+                  <h1>Paragraph About Us</h1>
+                </Col>
+                <Col>
+                  <Image src="brush-photo.jpg" fluid />
+                </Col>
+              </Row>
+            </Container>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Container>
+              <Row>
+                <Col>
+                  <Image src="rocks-photo.jpg" fluid />
+                </Col>
+                <Col>
+                  <h1>About our products</h1>
+                </Col>
+              </Row>
+            </Container>
+          </Carousel.Item>
+        </Carousel>
+        <Row>
+          <h1>Our Core product</h1>
+        </Row>
+      </Container>
+    </>
   )
 }
+
+
+// make a grid with content on one side and carousel on the other?
