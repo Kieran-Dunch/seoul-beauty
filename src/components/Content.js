1c5adab6b7b5b77e7f690aaa5829fda30461038e
Kieran Dunch <dunch.kieran@gmail.com>
2023-11-09 12:10:15 -0700

    flagship product draft

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,4 +1,4 @@
-import { Carousel, Col, Container, Image, Row } from "react-bootstrap";
+import { Card, Carousel, Col, Container, Image, Row } from "react-bootstrap";
 
 export default function Content() {
   return (
@@ -9,7 +9,7 @@ export default function Content() {
             <Container>
               <Row>
                 <Col>
-                  <h1>Paragraph About Us</h1>
+                  <h2>Paragraph About Us</h2>
                 </Col>
                 <Col>
                   <Image src="brush-photo.jpg" fluid />
@@ -24,16 +24,33 @@ export default function Content() {
                   <Image src="rocks-photo.jpg" fluid />
                 </Col>
                 <Col>
-                  <h1>About our products</h1>
+                  <h2>About our products</h2>
                 </Col>
               </Row>
             </Container>
           </Carousel.Item>
         </Carousel>
-        <Row>
-          <h1>Our Core product</h1>
+        <Row className="core-product align-items-center">
+          <Col md={6}>
+            <Card>
+              <Card.Title>
+                <h2 className="mt-5">
+                  The Subscription
+                </h2>
+              </Card.Title>
+              <Card.Body>
+                <h4 className="lh-lg my-3">
+                  Experience the epitome of skincare luxury with our flagship
+                  8-week subscription, a transformative journey towards radiant
+                  and healthy skin. Indulge in a curated selection of premium
+                  Korean skincare products meticulously designed to elevate your
+                  beauty regimen.
+                </h4>
+              </Card.Body>
+            </Card>
+          </Col>
         </Row>
-      </Container>
+      </Container >
     </>
   )
 }
