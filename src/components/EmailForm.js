547007765306aebf0307d413f1d78fe599ae5626
Kieran Dunch <dunch.kieran@gmail.com>
2023-11-10 14:37:26 -0700

    email form

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,5 +1,23 @@
+import { Button, Container, Form } from "react-bootstrap";
+
 export default function EmailForm() {
   return (
-    <div>EmailForm</div>
+    <Container className="body rounded my-5">
+      <Form className="p-3">
+        <h2>Join our email list to be the first to know!</h2>
+        <Form.Group controlId="formEmail" className="mb-3 text-start">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+        {/* <Form.Group controlId="formMessage" className="mb-3 text-start">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group> */}
+        <Button type='submit'>Submit</Button>
+      </Form>
+    </Container>
   )
 }
