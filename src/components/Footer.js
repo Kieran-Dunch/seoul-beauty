dd1da3a45625443c8f485e70afaf0143aba75dc0
Kieran Dunch <dunch.kieran@gmail.com>
2023-11-10 14:13:18 -0700

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,23 @@
+import { Col, Container, Nav, Row } from "react-bootstrap";
+
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer>
+      <Container fluid className="py-4">
+        <Row>
+          <Col className="px-4">
+            <span><em>© 2023 Seoul Beauty</em></span>
+          </Col>
+          <Col md={{ span: 3, offset: 6 }}>
+            <Nav>
+              <Nav.Link href="#" className="text-secondary">Home</Nav.Link>
+              <Nav.Link href="#" className="text-secondary">About</Nav.Link>
+              <Nav.Link href="#" className="text-secondary">Products</Nav.Link>
+              <Nav.Link href="#" className="text-secondary">Sign Up</Nav.Link>
+            </Nav>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   )
 }
